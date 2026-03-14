580610867d0e0b9eb71a5dcfc663339d1280958d
ky5697 -007 <ky5697@srmist.edu.in>
2026-03-14 23:40:06 +0530

    Koushik added leap year to git

---
+++ Level-3/LeapYearCheck.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class LeapYearCheck{
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a year: ");
+        int year = sc.nextInt();
+
+        if (year < 1582) {
+            System.out.println("The year " + year + " is not in the Gregorian calendar.");
+        } 
+        else {
+            if (year % 400 == 0) {
+                System.out.println("The year " + year + " is a Leap Year.");
+            } 
+            else if (year % 100 == 0) {
+                System.out.println("The year " + year + " is not a Leap Year.");
+            } 
+            else if (year % 4 == 0) {
+                System.out.println("The year " + year + " is a Leap Year.");
+            } 
+            else {
+                System.out.println("The year " + year + " is not a Leap Year.");
+            }
+        }
+
+        sc.close();
+    }
+}
